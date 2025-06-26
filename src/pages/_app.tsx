c616e7f76eba5d28da4b409decb0b46d3c70f6f1
Zhao Lixiuqi <e0774464@u.nus.edu>
2025-06-26 22:11:01 +0800

    Fix
    Fixx..

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,11 @@ import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import '../pages/Home.css';
+import '../pages/About.css';
+import '../pages/Projects.css';
+import '../pages/Contacts.css';
+import '../components/Navbar.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
