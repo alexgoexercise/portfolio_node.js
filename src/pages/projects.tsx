4d473a427435ea0ef7e538af91d0ee098bbfacbc
Zhao Lixiuqi <e0774464@u.nus.edu>
2025-07-02 11:54:52 +0800

    Update in Projects.css and projects.tsx
    fix the button position so that it doesn't block the title when viewing on devices with small screens

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -139,21 +139,23 @@ function AcademicPortfolio() {
           key={project.title} 
           className="project-card"
         >
-          {project.url && (
-            <div 
-              className="click-notice"
-              onClick={(e) => handleExternalClick(project, e)}
-              role="button"
-              tabIndex={0}
-              onKeyDown={(e) => handleExternalKeyDown(project, e)}
-            >
-              <span>{project.clickNotice}</span>
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M7 17L17 7M17 7H7M17 7V17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-              </svg>
-            </div>
-          )}
-          <h2>{project.title}</h2>
+          <div className="project-header">
+            <h2>{project.title}</h2>
+            {project.url && (
+              <div 
+                className="click-notice"
+                onClick={(e) => handleExternalClick(project, e)}
+                role="button"
+                tabIndex={0}
+                onKeyDown={(e) => handleExternalKeyDown(project, e)}
+              >
+                <span>{project.clickNotice}</span>
+                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M7 17L17 7M17 7H7M17 7V17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                </svg>
+              </div>
+            )}
+          </div>
           <div>
             {Array.isArray(project.description)
               ? project.description.map((line, idx) => <p key={idx}>{line}</p>)
